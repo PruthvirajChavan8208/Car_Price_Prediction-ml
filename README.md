676e8e23731ac89cbb4cd36cc3c2db900eb9a046
PruthvirajChavan8208 <pruthvirajchavan0104@gmail.com>
2026-05-08 13:06:11 +0530

    Final project files

---
+++ README.md
@@ -0,0 +1,24 @@
+# Car Price Prediction Project
+
+This is a Machine Learning web application that predicts the price of a car based on various features using a trained model.
+
+## 🚀 Features
+* Web Interface: Built with Streamlit for a user-friendly experience.
+* Model: Uses a trained regression model (CPQ.pkl).
+* Real-time Prediction: Enter car details and get an instant price estimate.
+
+## 🛠️ Tech Stack
+* Language: Python
+* Libraries: Pandas, NumPy, Scikit-learn, Streamlit
+* Deployment: GitHub & Streamlit Cloud
+
+## 📂 Project Structure
+* app.py: The main script for the Streamlit web interface.
+* CPQ.pkl: The trained machine learning model.
+* cleaned_data: The dataset used for the project.
+* requirements.txt: List of Python libraries needed to run the app.
+
+## 🔧 How to Run Locally
+1. Clone the repository:
+   `bash
+   git clone <your-repo-link>
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import pickle as pkl
+
+
+st.title("Car Price Prediction App")
+st.write("Welcome to pruthviraj chavan's Project")
+
+model=pkl.load(open("CPQ.pkl","rb+"))
+ds=pd.read_csv("cleaned_data.csv")
+
+companies=sorted(ds["company"].unique())
+fuel_types=sorted(ds["fuel_type"].unique())
+
+years=[]
+for i in range(1995,2025):
+    years.append(i)
+
+company=st.sidebar.selectbox("Select Company:",companies,key="company")
+names=sorted(ds[ds["company"]==company]["name"].unique())#to get name of that company car
+name=st.sidebar.selectbox("Select Name:",names,key="name")#Column in app
+year=st.sidebar.selectbox("Select year",years)
+kms_driven=st.sidebar.text_input("Enter the Kms Driven","10000")
+fuel_type=st.sidebar.selectbox("Select Fuel Type",fuel_types,key="fuel_type")
+st.write("You selected",company,name)
+
+if st.sidebar.button("Predict"):
+    columns=["company","name","year","kms_driven","fuel_type"]
+    data=[[company,name,year,kms_driven,fuel_type]]
+    st.write("You Provide Following Information:")
+    st.write("Comapny:",company)
+    st.write("Name:",name)
+    st.write("Year:",year)
+    st.write("Total Kms_driven:",kms_driven)
+    st.write("Fuel Type:",fuel_type)
+    myinput=pd.DataFrame(data=data,columns=columns)
+    result=model.predict(myinput)
+    st.write("Predicted price:",result[0,0])
